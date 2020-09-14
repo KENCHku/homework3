2c5b7db78d32c092318e87fa1e8eb4a5a356a0ac
KENCHku <kench.mkk3@gmail.com>
2020-09-14 16:05:35 +0600

    HW_1.2

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,40 @@ public class Main {
         double kol = 0;
         boolean isOtr = false;
         double[] mas1 = {8.0, -2.0, 4.0, -6.0, 5.0, -8.0, 3.0, -6.0, -9.0, -3.0, 2.0, -7.0, -4.0, 1.0};
+        for (double mas : mas1) {
+            if (mas < 0) {
+                isOtr = true;
+            } else {
+                if (isOtr) {
+                    kol++;
+                    sum += mas;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas1));
+        System.out.println(sum / kol);
 
-        bubbleSort(mas1);
+        sortArray(mas1);
 
         System.out.println(Arrays.toString(mas1));
 
     }
 
-        public static void bubbleSort(double[] arr) {
-
-            for (int i = arr.length - 1; i > 0; i--) {
-                for (int j = 0; j < i; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        double tmp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = tmp;
-                    }
+    public static void sortArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            double minValue = arr[i];
+            for (int i1 = i; i1 < arr.length; i1++) {
+                if (minValue > arr[i1]){
+                    double d = minValue;
+                    minValue = arr[i1];
+                    arr[i1] = d;
                 }
             }
+            arr[i] = minValue;
         }
+    }
+
+
 }
 
 
